c1cd74afcaaf5fc446df09cb530517ab62974bed
abdifatah-bashi <abdifatah.bashi@nav.no>
2024-09-11 16:09:39 +0200

    Start Fabrikk- og SøknadMottak

--- mediator/src/main/kotlin/no/nav/dagpenger/rapportering/fabrikk/mediator/ApplicationBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/rapportering/fabrikk/mediator/ApplicationBuilder.kt
@@ -1,6 +1,9 @@
 package no.nav.dagpenger.rapportering.fabrikk.mediator
 
+import mu.KotlinLogging
 import no.nav.dagpenger.rapportering.fabrikk.mediator.api.internalApi
+import no.nav.dagpenger.rapportering.fabrikk.mediator.tjenester.FabrikkMottak
+import no.nav.dagpenger.rapportering.fabrikk.mediator.tjenester.SoknadMottak
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 
@@ -23,5 +26,14 @@ internal class ApplicationBuilder(
     }
 
     override fun onStartup(rapidsConnection: RapidsConnection) {
+        val rapporteringMediator = RapporteringMediator(rapidsConnection)
+        FabrikkMottak(rapidsConnection, rapporteringMediator).also {
+            logger.info { "Startet FabrikkMottak" }
+        }
+        SoknadMottak(rapidsConnection, rapporteringMediator)
+    }
+
+    private companion object {
+        private val logger = KotlinLogging.logger {}
     }
 }
